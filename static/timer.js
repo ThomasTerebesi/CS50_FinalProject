167cdedf32e29f2d384120000d1008c511ff96aa
Thomas Terebesi <thomas@pop-os.localdomain>
2022-05-16 21:19:05 +0200

    Added presets to timer page

--- static/timer.js
+++ static/timer.js
@@ -1,29 +1,25 @@
-const set_button = document.getElementById('set');
-set_button.addEventListener('click', timer, false);
-
-
-const pause_button = document.getElementById('pause');
-pause_button.addEventListener('click', togglePause, false);
-
+// Get seconds from document and store them as integer values
+const userSeconds = parseInt(document.getElementById("sec").value) + (parseInt(document.getElementById("min").value) * 60);
 
 let timerInterv;
 let isPaused = false;
 
+const set_button = document.getElementById('set');
+set_button.addEventListener('click', startTimer(userSeconds), false);
 
-function timer() {
+function startTimer(totalSeconds) {
     // Timer based on the following tutorial: https://youtu.be/x7WJEmxNlEs
+
+    // Unpause the timer on start if it was paused before
+    if (isPaused) {
+        isPaused = false;
+        pause_button.innerHTML = 'Pause';
+    }
 
     // If another interval is already set, clear that interval before starting a new one
     if (timerInterv) {
         clearInterval(timerInterv);
     }
-
-    // Get minutes and seconds from document and store them as integer values
-    const userMinutes = parseInt(document.getElementById("min").value);
-    const userSeconds = parseInt(document.getElementById("sec").value);
-
-    // Calculate total time in seconds
-    let time = (userMinutes * 60) + userSeconds;
 
     // Get the element that is going to be changed each second
     const countdownElement = document.getElementById('timer');
@@ -37,9 +33,9 @@ function timer() {
         // Check if the countdown is paused
         if(!isPaused) {
 
-            // Resolve total time back to minutes and seconds
-            let minutes = Math.floor(time / 60);
-            let seconds = time % 60;
+            // Resolve totalSeconds back to minutes and seconds
+            let minutes = Math.floor(totalSeconds / 60);
+            let seconds = totalSeconds % 60;
 
             // If minutes or seconds have less than two digits, add a leading zero
             minutes = minutes < 10 ? '0' + minutes : minutes; 
@@ -48,13 +44,13 @@ function timer() {
             // Update the element
             countdownElement.innerHTML = `${minutes}:${seconds}`;
             
-            // Check if time is still greater than zero
-            if (time > 0) {
-                time--;
+            // Check if totalSeconds is still greater than zero
+            if (totalSeconds > 0) {
+                totalSeconds--;
             }
             else {
                 // TODO: Play sound? Alert? Flash "00:00" a couple of times?
-                alert("TIME'S UP!");
+                alert("totalSeconds'S UP!");
                 clearInterval(timerInterv);
                 return;
             }
@@ -62,6 +58,9 @@ function timer() {
     }
 }
 
+
+const pause_button = document.getElementById('pause');
+pause_button.addEventListener('click', togglePause, false);
 
 function togglePause() {
     if (isPaused) {
@@ -72,3 +71,17 @@ function togglePause() {
         pause_button.innerHTML = 'Continue';
     }
 }
+
+const preset_buttons = document.getElementsByName('preset');
+console.log(preset_buttons);
+
+preset_buttons.forEach(element => {
+    element.addEventListener('click', event => {
+        if (isPaused) {
+            isPaused = false;
+            pause_button.innerHTML = 'Pause';
+        }
+
+        startTimer(element.value);
+    })
+})
